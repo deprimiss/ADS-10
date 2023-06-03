0ca53e8be9d5c3b86301973bbcc7cf9ab793ab1e
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-06-04 01:17:28 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -4,25 +4,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 class Tree {
 private:
     struct Node {
         std::vector<Node*> vct;
         char ch;
     };
-    Node* rt;
+    Node* root;
     std::vector<std::vector<char>> allpms;
-    void create(Node* root, const std::vector<char>& in) {
-        for (char ch : in) {
+    void create(Node* rt, const std::vector<char>& v) {
+        for (char ch : v) {
             Node* temp = new Node;
             temp->ch = ch;
-            root->vct.push_back(temp);
-            std::vector<char> ost = in;
+            rt->vct.push_back(temp);
+            std::vector<char> ost = v;
             ost.erase(std::find(ost.begin(), ost.end(), ch));
             create(temp, ost);
         }
     }
     void createallperms(Node* rt, std::vector<char> v) {
+        if (!(rt->ch == ' ')) {
+            v.push_back(rt->ch);
+        }
         if (!rt->vct.empty()) {
             for (Node* next : rt->vct) {
                 createallperms(next, v);
@@ -31,21 +35,18 @@ private:
         else {
             allpms.push_back(v);
         }
-        if (!(rt->ch == ' ')) {
-            v.push_back(rt->ch);
-        }
     }
 
 public:
     std::vector<std::vector<char>> getAllPerms() const {
         return allpms;
     }
-    explicit Tree(std::vector<char> in) {
-        rt = new Node;
-        rt->ch = ' ';
-        create(rt, in);
+    explicit Tree(std::vector<char> v) {
+        root = new Node;
+        root->ch = ' ';
+        create(root, v);
         std::vector<char> v;
-        createallperms(rt, v);
+        createallperms(root, v);
     }
 };
 #endif  // INCLUDE_TREE_H_
